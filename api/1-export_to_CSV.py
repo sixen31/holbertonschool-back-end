71ef98ffd1debd685de3922f0075263d0a16124a
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-24 03:39:50 -0700

    Using what you did in the task #0, extend your Python script to export data in the CSV format

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script Python qui pour un ID d'employé donné renvoie toutes ses tâches à faire et les exporte au format CSV"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         sys.argv[1], timeout=5)
+    names = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                         sys.argv[1], timeout=5)
+
+    json_todo = to_do.json()
+    json_names = names.json()
+
+    with open('{}.csv'.format(sys.argv[1]), mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in json_todo:
+            writer.writerow([sys.argv[1], json_names['username'], task['completed'], task['title']])
