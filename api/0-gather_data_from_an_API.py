d971d93bf2e4120062beb3a821efbbcf7a261359
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-24 06:35:44 -0700

    reprise

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,6 +4,7 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
     to_do = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
                          sys.argv[1], timeout=5)
@@ -19,19 +20,12 @@ if __name__ == "__main__":
     titles_completed = []
     for task in json_todo:
         all_tasks += 1
-        if task["completed"]:
+        if task["completed"] is True:
             tasks_completed += 1
             titles_completed.append(task["title"])
 
-    employee_name = json_names['name']
-    employee_id = sys.argv[1]
-
-    # Formatting the first line correctly
-    first_line = "Employee {} is done with tasks({}/{}):".format(
-        employee_name, tasks_completed, all_tasks)
-    formatted_first_line = first_line[:26] + "OK" if len(first_line) == 26 else "Incorrect"
-
-    print(formatted_first_line)
+    print('L\'employé {} a terminé ({}/{}) de ses tâches :'
+          .format(json_names['name'], tasks_completed, all_tasks))
 
     for title_task in titles_completed:
-        print('\t{}'.format(title_task))
+        print('\t {}'.format(title_task))
